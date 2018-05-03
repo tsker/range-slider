5b2306a9b12ea24b9c81a66ff8347ec62ec28de2
luofei <477800969@qq.com>
2018-05-03 17:42:10 +0800

    optimze

--- range-slider.js
+++ range-slider.js
@@ -21,9 +21,9 @@ class RangeSlider {
 
 	init() {
 		this.handleDragstart = this.handleDragstart.bind(this);
-		this.handleDrag = this.handleDrag.bind(this);
-		this.handleDragover = this.handleDragover.bind(this);
-		this.resize = this.resize.bind(this);
+		this.handleDrag      = this.handleDrag.bind(this);
+		this.handleDragover  = this.handleDragover.bind(this);
+		this.resize          = this.resize.bind(this);
 
 		this.resize();
 		this.renderProcess();
@@ -35,7 +35,7 @@ class RangeSlider {
 	getCurrentValue() {
 		return this.points.map((p) => p.value);
 	}
-	saveValue(value) {
+	safeValue(value) {
 		let { min, max } = this.opts;
 		return Math.max(min, Math.min(max, value));
 	}
@@ -43,38 +43,40 @@ class RangeSlider {
 		let range = this.points.map((p) => p.position);
 		let left, right;
 		if (range.length <= 1) {
-			left = 0;
+			left  = 0;
 			right = 100 - range[0];
 		} else {
-			left = Math.min(...range);
+			left  = Math.min(...range);
 			right = 100 - Math.max(...range);
 		}
-		this.process.style.left = left + '%';
+		this.process.style.left  = left + '%';
 		this.process.style.right = right + '%';
 	}
 	positionPoint(point, value) {
 		let { min, step } = this.opts;
-		let offsetValue = Math.ceil((value - min) / step) / this.pointCount * 100;
+		let offsetValue   = Math.ceil((value - min) / step) / this.pointCount * 100;
 
-		point.value = value;
-		point.position = offsetValue;
+		point.value      = value;
+		point.position   = offsetValue;
 		point.style.left = offsetValue + '%';
 
 		this.opts.onPointPosition(offsetValue);
 	}
-	setValue(value) {
-		this.points.forEach((point, index) => {
-			this.positionPoint(point, this.saveValue(value[index]));
+	setValue(values) {
+		let safeValues = this.points.map((point, index) => {
+			let value = this.safeValue(values[index]);
+			this.positionPoint(point, value);
+			return value;
 		});
 
 		this.updateProcess();
-		this.opts.onChange(this.getCurrentValue());
+		this.opts.onChange(safeValues);
 	}
 
 	resize() {
 		let { max, min, step, el } = this.opts;
 		this.pointCount = Math.ceil((max - min) / step);
-		this.stepWidth = el.offsetWidth / this.pointCount;
+		this.stepWidth  = el.offsetWidth / this.pointCount;
 	}
 
 	handleDragstart(e) {
@@ -98,7 +100,7 @@ class RangeSlider {
 		let increment =
 			Math.ceil((Math.abs(moveX) - this.stepWidth / 2) / this.stepWidth) * this.opts.step;
 		let dir = moveX > 0 ? 1 : -1;
-		let newValue = this.saveValue(this.movePoint.value + increment * dir);
+		let newValue = this.safeValue(this.movePoint.value + increment * dir);
 
 		if (newValue !== this.movePoint.el.value) {
 			this.positionPoint(this.movePoint.el, newValue);
@@ -129,17 +131,17 @@ class RangeSlider {
 	renderProcess() {
 		let processNode = document.createElement('div');
 		processNode.className = 'y-process';
-		this.process = processNode;
+		this.process          = processNode;
 
 		this.opts.el.appendChild(processNode);
 	}
 	renderPoints() {
 		this.points = this.opts.value.map((v, index) => {
-			let pointNode = document.createElement('div');
-			pointNode.className = 'y-point';
-			pointNode.index = index;
+			let pointNode           = document.createElement('div');
+			    pointNode.className = 'y-point';
+			    pointNode.index     = index;
 
-			this.positionPoint(pointNode, this.saveValue(v));
+			this.positionPoint(pointNode, this.safeValue(v));
 			this.opts.el.appendChild(pointNode);
 
 			return pointNode;
